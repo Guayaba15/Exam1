426fae76b5d649f4623b3480b8ab634ac95a5125
Arturo <jasonvores@outlook.es>
2023-11-10 16:47:46 -0600

    v2

--- garciaProyect/settings.py
+++ garciaProyect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h@@rmfxt_#1rwpqkmlxfm#==0vlr%m()1j2ysuc4hb^!n$o4be
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.118.206.51', 'localhost', '127.0.0.1']
 
 
 # Application definition
